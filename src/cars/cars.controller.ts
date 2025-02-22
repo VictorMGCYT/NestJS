f2fbb6279fab7513f0a0caee86116120ff89e4fe
VictorMGCYT <manuelvgc2233@gmail.com>
2025-02-22 08:46:31 -0600

    CRUD don't validated
    22/02/2025

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post } from '@nestjs/common';
 import { CarsService } from './cars.service';
 
 @Controller('cars')
@@ -17,9 +17,27 @@ export class CarsController {
 
     // Obtener carro por su ID
     @Get(':id')     //Aquí estamos decorando el parametro ID para que sepa que es el parametro de la URL
-    getCarById(@Param('id') id: string){
-
+    getCarById(@Param('id', ParseIntPipe) id: number){
         return this.carsServie.findOneById(Number(id));
     }
 
+    @Post()
+    createCar( @Body() body: any ){
+        return body;
+    }
+
+    @Patch(':id')
+    updateCar( 
+        @Param('id', ParseIntPipe) id: number,
+        @Body() body: any ){
+        return body;
+    }
+
+    @Delete(':id')
+    deleteCar( @Param('id', ParseIntPipe) id: number ){
+        return {
+            methos: "Delete",
+            msg: `Deleted car with id: ${id}`
+        };
+    }
 }
